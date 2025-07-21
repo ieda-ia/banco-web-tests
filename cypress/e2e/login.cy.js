d575efe25413bbb911cce9eb9d06d0c5f86b3d92
ieda-ia <iedabcc@gmail.com>
2025-07-21 10:29:18 -0300

    Adicionei dados provenientes do fixture e alterei o .gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,11 +6,12 @@ describe('Login', () => {
   })
   it('Login com dados VÁLIDOS deve permitir entrada no sistema', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais_validas => {
+      cy.get('#username').click().type(credenciais_validas.valida.usuario)
+      cy.get('#senha').click().type(credenciais_validas.valida.senha)
+        
     cy.screenshot('apos-preencher-dados-validos')
-    cy.get('#login-section > .btn').click()
-   
+    cy.get('#login-section > .btn').click()   
     cy.screenshot('apos-clicar-botao-entrar')
 
     //Asserts
@@ -19,9 +20,11 @@ describe('Login', () => {
 
   it('Login com dados INVÁLIDOS deve permitir entrada no sistema', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
-    cy.contains('button', 'Entrar').click()
+    cy.fixture('credenciais').then(credenciais_invalidas => {
+      cy.get('#username').click().type(credenciais_invalidas.invalida.usuario)
+      cy.get('#senha').click().type(credenciais_invalidas.invalida.senha)
+    })
+      cy.contains('button', 'Entrar').click()
 
     //Asserts
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
