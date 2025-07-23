2eb7602c2cde0164aba3b6e9b9c44a08b749e8f6
ieda-ia <iedabcc@gmail.com>
2025-07-23 15:40:16 -0300

    Adicionando o script de testes de transferencias

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,14 +9,15 @@ describe('Login', () => {
       cy.fixture('credenciais').then(credenciais_validas => {
         cy.get('#username').click().type(credenciais_validas.valida.usuario)
         cy.get('#senha').click().type(credenciais_validas.valida.senha)
-          
+      })    
+      
       cy.screenshot('apos-preencher-dados-validos')
-      cy.get('#login-section > .btn').click()   
+      cy.contains('button', 'Entrar').click()   
       cy.screenshot('apos-clicar-botao-entrar')
-
+      
       //Asserts
       cy.contains('h4', 'Realizar Transferência').should('be.visible')
-    })
+    
   })
 
   it('Login com dados INVÁLIDOS deve permitir entrada no sistema', () => {
